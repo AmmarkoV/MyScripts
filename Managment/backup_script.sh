c6cd6f8522685488d2cfff985934280bdb718ef0
Ammar Qammaz <ammarkov@gmail.com>
2013-01-01 05:16:27 +0200

    ...

---
+++ Managment/backup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /
+mysqldump -u root -pPASS_HERE --all-databases | gzip > /home/ammar/mysql/mysql_`date +"%m-%d-%Y"`.sql.gz
+rm /home/ammar/backup.*
+sudo tar -cvpj /home/ | sudo split -d -b 4000m - backup.tar.bz2.
+sudo mv backup.* ammar/
+#sudo tar cvf backup.tar . 
+#sudo mv backup.tar /home/ammar/backup.tar
+exit 0
