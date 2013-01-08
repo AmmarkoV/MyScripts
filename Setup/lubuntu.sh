f63590dd6969b47f1a4046d2de78676173f5913e
Ammar Qammaz <ammar@ammar-laptop.(none)>
2013-01-08 20:04:32 +0200

    added autostart things

--- Setup/lubuntu.sh
+++ Setup/lubuntu.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 echo "LUbuntu handy Packages automation "
 
-sudo apt-add-repository ppa:xorg-edgers/ppa
-sudo apt-add-repository ppa:ubuntu-x-swat/x-updates
-sudo apt-get update
-
-
 BASICAPPS="firefox thunderbird vlc pidgin mumble gimp audacity audacious libreoffice synaptic catfish usb-creator-gtk vino xtightvncviewer baobab"
 MOREAPPS="glabels freemind gtg gnotime gtk-recordmydesktop units firestarter qrencode"
 COMPATIBILITY="wine winetricks dosbox samba system-config-samba chntpw"
-ADVLIBS="sysv-rc-conf xbacklight festival imagemagick numlockx gxmessage libnotify-bin htop gtkperf traceroute"
-CODECS="ubuntu-restricted-extras  libdvdread4 pavucontrol beep ffmpeg  mplayer smplayer p7zip-full"
+ADVLIBS="sysv-rc-conf festival imagemagick numlockx gxmessage libnotify-bin htop gtkperf traceroute"
+CODECS="ubuntu-restricted-extras pavucontrol beep ffmpeg  mplayer smplayer"
 
 sudo apt-get install $BASICAPPS $MOREAPPS $ADVLIBS $COMPATIBILITY $ADVLIBS $CODECS         
 
 
 #esddsp festival palia
 echo "Installation Complete" |  festival --tts
-
-sudo /usr/share/doc/libdvdread4/install-css.sh
 
 #sudo echo "default-sample-rate=48000" >> /etc/pulse/daemon.conf
 #sudo echo "frequency=48000" >> /etc/openal/alsoft.conf
@@ -32,6 +25,38 @@ else
    sudo sh -c 'echo "@setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,gr" >>/etc/xdg/lxsession/Lubuntu/autostart' 
 fi
 
-sudo apt-get remove abiword ace-of-penguins
 
-echo "Config
+if [ -f ~/.config/autostart/autostart.desktop ]
+then 
+ echo "Found per-user autostart shortcut"
+else 
+ echo "Generating new per-user autostart shortcut"
+ echo "[Desktop Entry]" > ~/.config/autostart/autostart.desktop
+ echo "Type=Application" >> ~/.config/autostart/autostart.desktop
+ echo "Name=MyThings" >> ~/.config/autostart/autostart.desktop
+ echo "Exec=~/.autostart.sh" >> ~/.config/autostart/autostart.desktop
+fi
+
+if [ -f ~/.autostart.sh ]
+then 
+ echo "Found per-user autostart bash script"
+else 
+ echo "Generating new per-user autostart bash script"
+ echo "#!/bin/bash" > ~/.autostart.sh
+ echo "setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,gr" >> ~/.autostart.sh 
+ echo "pidgin&" >> ~/.autostart.sh 
+ echo "thunderbird&" >> ~/.autostart.sh 
+ echo "firefox&" >> ~/.autostart.sh 
+ echo "mumble&" >> ~/.autostart.sh 
+ echo "audacious&" >> ~/.autostart.sh 
+ echo "exit 0" >> ~/.autostart.sh 
+fi
+
+
+
+sudo apt-get remove abiword
+
+echo "Configuration Complete" |  festival --tts
+
+# sensors-applet
+exit 0
