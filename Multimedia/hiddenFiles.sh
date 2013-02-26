f3e2efc915beeed00bed224fbf3afb6740eb1eaa
Ammar Qammaz <ammarkov@gmail.com>
2013-02-26 17:16:22 +0200

    ...

--- Multimedia/hiddenFiles.sh
+++ Multimedia/hiddenFiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+CURRENTDIR=`pwd`
+echo "Switching to $1 dir"
+cd $1
 find -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.jpeg" -o -exec 7z x "{}" -o/tmp/ \;
+cd $CURRENTDIR
+
 
 exit 0
