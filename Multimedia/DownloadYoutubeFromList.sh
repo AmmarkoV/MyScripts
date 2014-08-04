4e8310cfab0dca7f88a447bd1a9cd6b5a230284f
Ammar Qammaz <ammarkov@gmail.com>
2014-08-04 16:42:11 +0300

    ...

--- Multimedia/DownloadYoutubeFromList.sh
+++ Multimedia/DownloadYoutubeFromList.sh
@@ -2,8 +2,14 @@
 
 #cat $1 | grep youtube | sort | xargs -L 1 sh -c '~/MyScripts/Multimedia/DownloadYoutubeVid.pl "$0"'
 
+
+#sudo curl https://yt-dl.org/downloads/2014.08.02.1/youtube-dl -o /usr/bin/youtube-dl
+#sudo chmod a+x /usr/bin/youtube-dl
+#sudo youtube-dl -U
+
+
 sudo apt-get install youtube-dl
-cat $1 | grep youtube | sort | xargs -L 1 sh -c 'youtube-dl "$0"'
+cat $1 | grep youtube | sort | xargs -L 1 sh -c 'youtube-dl --extract-audio --audio-format mp3 -l "$0"'
 
 
 exit 0
