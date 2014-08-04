aaa5cb468036fd5bc0ffe6ab79f31f7fd9c9fdc0
Ammar Qammaz <ammarkov@gmail.com>
2014-08-04 15:54:07 +0300

    youtube script now uses youtube-dl

--- Multimedia/DownloadYoutubeFromList.sh
+++ Multimedia/DownloadYoutubeFromList.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cat $1 | grep youtube | sort | xargs -L 1 sh -c '~/Scripts/Multimedia/DownloadYoutubeVid.pl "$0"'
+#cat $1 | grep youtube | sort | xargs -L 1 sh -c '~/MyScripts/Multimedia/DownloadYoutubeVid.pl "$0"'
+
+sudo apt-get install youtube-dl
+cat $1 | grep youtube | sort | xargs -L 1 sh -c 'youtube-dl "$0"'
+
 
 exit 0
