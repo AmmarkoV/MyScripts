ba74f06632c6ce8b1ec7f45c8f98de2dbf97aab1
Ammar Qammaz <ammarkov@gmail.com>
2014-11-15 19:15:41 +0200

    ...

--- Setup/switchGL.sh
+++ Setup/switchGL.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+#KERNEL=="nvidia_uvm", RUN+="/usr/bin/bash -c '/bin/mknod -m 660 /dev/nvidia-uvm c $(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0; /bin/chgrp video /dev/nvidia-uvm'"
+
+#Edit the file /etc/modprobe.d/bumblebee.conf
+#alias nvidia-uvm nvidia-340-uvm
+
+
 sudo update-alternatives --config x86_64-linux-gnu_gl_conf
 exit 0
