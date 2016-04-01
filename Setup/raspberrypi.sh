63754b95e7c1e59b8c00eaff66c3a87bb9e26c24
Ammar Qammaz <ammarkov@gmail.com>
2016-04-01 17:51:12 +0300

    ..

--- Setup/raspberrypi.sh
+++ Setup/raspberrypi.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install kodi
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt-get install kodi git cmake build-essential vlc smplayer audacious libsdl1.2-dev
+
+cd ~/
+mkdir Programs
+cd Programs/ 
+
+
+git clone https://github.com/libretro/pcsx_rearmed
+cd pcsx_rearmed
+git submodule init && git submodule update
+./configure
+
+cd ~/Programs
+git clone git://github.com/AmmarkoV/AmmarServer
+git clone git://github.com/AmmarkoV/FlashySlideshows
 
 
 exit 0
