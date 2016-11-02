a68f079dd4aa8d6932b82e40ce04cc8dd953635c
Ammar Qammaz <ammarkov@gmail.com>
2016-11-02 17:31:16 +0200

    ...

--- Setup/switchGL.sh
+++ Setup/switchGL.sh
@@ -7,4 +7,28 @@
 
 
 sudo update-alternatives --config x86_64-linux-gnu_gl_conf
+
+#  Selection    Path                                       Priority   Status
+#------------------------------------------------------------
+#  0            /usr/lib/nvidia-361/ld.so.conf              8604      auto mode
+#  1            /usr/lib/nvidia-361-prime/ld.so.conf        8603      manual mode
+#  2            /usr/lib/nvidia-361/ld.so.conf              8604      manual mode
+#* 3            /usr/lib/x86_64-linux-gnu/mesa/ld.so.conf   500       manual mode
+
+
+#sudo nano /etc/modprobe.d/bumblebee.conf
+#make sure the nvidia driver is blacklisted there
+
+
+#sudo nano /etc/bumblebee/bumblebee.conf
+#KernelDriver=nvidia-361
+#PMMethod=auto
+#LibraryPath=/usr/lib/nvidia-361:/usr/lib32/nvidia-361
+#XorgModulePath=/usr/lib/nvidia-361/xorg,/usr/lib/xorg/modules
+
+
+
+
+
+
 exit 0
