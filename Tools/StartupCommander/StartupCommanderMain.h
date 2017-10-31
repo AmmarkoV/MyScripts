85134ec78ba5d1de8e8f7594e0c675ac8c376606
Ammar Qammaz <ammarkov@gmail.com>
2017-10-31 17:53:57 +0200

    adding a tool to fast choose what to startup..

---
+++ Tools/StartupCommander/StartupCommanderMain.h
@@ -0,0 +1,56 @@
+/***************************************************************
+ * Name:      StartupCommanderMain.h
+ * Purpose:   Defines Application Frame
+ * Author:    Ammar Qammaz (ammarkov+rgbd@gmail.com)
+ * Created:   2017-10-31
+ * Copyright: Ammar Qammaz (http://ammar.gr)
+ * License:
+ **************************************************************/
+
+#ifndef STARTUPCOMMANDERMAIN_H
+#define STARTUPCOMMANDERMAIN_H
+
+//(*Headers(StartupCommanderFrame)
+#include <wx/gauge.h>
+#include <wx/button.h>
+#include <wx/menu.h>
+#include <wx/statusbr.h>
+#include <wx/frame.h>
+//*)
+
+class StartupCommanderFrame: public wxFrame
+{
+    public:
+
+        StartupCommanderFrame(wxWindow* parent,wxWindowID id = -1);
+        virtual ~StartupCommanderFrame();
+
+    private:
+
+        //(*Handlers(StartupCommanderFrame)
+        void OnQuit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+        void OnButtonExitClick(wxCommandEvent& event);
+        void OnButtonStartupClick(wxCommandEvent& event);
+        //*)
+
+        //(*Identifiers(StartupCommanderFrame)
+        static const long ID_BUTTON1;
+        static const long ID_BUTTON2;
+        static const long ID_GAUGE1;
+        static const long idMenuQuit;
+        static const long idMenuAbout;
+        static const long ID_STATUSBAR1;
+        //*)
+
+        //(*Declarations(StartupCommanderFrame)
+        wxStatusBar* StatusBar1;
+        wxGauge* GaugeTimeout;
+        wxButton* ButtonExit;
+        wxButton* ButtonStartup;
+        //*)
+
+        DECLARE_EVENT_TABLE()
+};
+
+#endif // STARTUPCOMMANDERMAIN_H
