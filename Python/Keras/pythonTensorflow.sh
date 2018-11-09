148d3f87a3fb45a7c75eaa189872e151eabd7bc8
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-11-09 17:53:20 +0200

    ...

--- Python/Keras/pythonTensorflow.sh
+++ Python/Keras/pythonTensorflow.sh
@@ -5,9 +5,11 @@ python3 -m venv tensorflow
 source tensorflow/bin/activate
 pip install numpy
 pip install tensorflow
+#pip install tensorflow_gpu
 pip install keras 
 pip install pillow
 pip install matplotlib 
+pip install pydot
 
 
 exit 0
