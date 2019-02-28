b29e1d9deb4299de3de135fe44eca505c1b57a0f
Ammar Qammaz <ammarkov@gmail.com>
2019-02-28 18:11:57 +0200

    adding a script to get/build meshroom..

---
+++ Setup/getMeshroom.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ~/Documents/3dParty
+
+sudo apt-get install libpng-dev libjpeg-dev libtiff-dev libxxf86vm1 libxxf86vm-dev libxi-dev libxrandr-dev libopenimageio-dev libceres-dev graphviz
+
+#-------------------------------------------
+#                  GEOGRAM 
+#-------------------------------------------
+git clone https://github.com/alicevision/geogram
+cd geogram/
+./configure
+cd build/Linux64-gcc-dynamic-Release
+make 
+cd ..
+cd ..
+cd ..
+
+#-------------------------------------------
+#                ALICEVISION 
+#-------------------------------------------
+git clone --recursive git://github.com/alicevision/AliceVision
+cd AliceVision
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DALICEVISION_BUILD_TESTS=ON -DALICEVISION_BUILD_EXAMPLES=ON ../AliceVision
+cd ..
+cd ..
+
+
+#-------------------------------------------
+#                  MESHROOM 
+#-------------------------------------------
+git clone --recursive git://github.com/alicevision/meshroom
+cd meshroom
+#pip install pyside2 pyqt5 --user
+#pip install -r requirements.txt -r dev_requirements.txt
+cd ..
+
+#QML2_IMPORT_PATH=/path/to/qmlAlembic/install/qml
+#QT_PLUGIN_PATH=/path/to/QtOIIO/install
+#QML2_IMPORT_PATH=/path/to/QtOIIO/install/qml
+
+
+#-------------------------------------------
+
+ALICEVISION_SENSOR_DB=/home/ammar/Documents/3dParty/AliceVision/src/aliceVision/sensorDB
+ALICEVISION_VOCTREE=/home/ammar/Documents/3dParty/AliceVision/src/aliceVision/voctree
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ammar/Documents/3dParty/AliceVision 
+PATH=$PATH:/home/ammar/Documents/3dParty/AliceVision 
+cd meshroom
+PYTHONPATH=$PWD python meshroom/ui
+
+exit 0
