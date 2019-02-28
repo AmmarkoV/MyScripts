abe2bdb6feedf2f41ec5e2e1b8968bd104c70d46
Ammar Qammaz <ammarkov@gmail.com>
2019-03-01 00:40:33 +0200

    ...

--- Setup/getMeshroom.sh
+++ Setup/getMeshroom.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-mkdir -p ~/Documents/3dParty
-cd ~/Documents/3dParty
+INSTALL_DIR="~/Documents/3dParty"
 
-sudo apt-get install libpng-dev libjpeg-dev libtiff-dev libxxf86vm1 libxxf86vm-dev libxi-dev libxrandr-dev libopenimageio-dev libceres-dev graphviz
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+
+sudo apt-get install libpng-dev libjpeg-dev libtiff-dev libxxf86vm1 libxxf86vm-dev libxi-dev libxrandr-dev libceres-dev graphviz #libopenimageio-dev 
+
+#-------------------------------------------
+#                  OpenImageIO 
+#-------------------------------------------
+git clone https://github.com/OpenImageIO/oiio
+cd oiio
+OIIODIR=`pwd`
+mkdir build && cd build
+cmake .. 
+make
+sudo make install
+cd $INSTALL_DIR
 
 #-------------------------------------------
 #                  GEOGRAM 
@@ -12,11 +26,10 @@ git clone https://github.com/alicevision/geogram
 cd geogram/
 ./configure
 cd build/Linux64-gcc-dynamic-Release
+GEOGRAMDIR=`pwd`
 make 
 sudo make install
-cd ..
-cd ..
-cd ..
+cd $INSTALL_DIR
 
 #-------------------------------------------
 #                ALICEVISION 
@@ -24,9 +37,9 @@ cd ..
 git clone --recursive git://github.com/alicevision/AliceVision
 cd AliceVision
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DALICEVISION_BUILD_TESTS=ON -DALICEVISION_BUILD_EXAMPLES=ON ../AliceVision
-cd ..
-cd ..
+cmake -DCMAKE_BUILD_TYPE=Release -DALICEVISION_BUILD_TESTS=ON -DALICEVISION_BUILD_EXAMPLES=ON -DGEOGRAM_GFX_LIBRARY=$GEOGRAMDIR/lib/libgeogram_gfx.so -DGEOGRAM_GLFW3_LIBRARY=$GEOGRAMDIR/lib/libglfw.so.3 -DGEOGRAM_INCLUDE_DIR=$GEOGRAMDIR/src/lib -DGEOGRAM_LIBRARY=$GEOGRAMDIR/lib/libgeogram.so -DOPENIMAGEIO_LIBRARY=$OIIODIR/build/src/libOpenImageIO/libOpenImageIO.so  -DOPENIMAGEIO_INCLUDE_DIR=$OIIODIR/src/include  ..
+make
+cd $INSTALL_DIR
 
 
 #-------------------------------------------
@@ -36,13 +49,13 @@ git clone --recursive git://github.com/alicevision/meshroom
 cd meshroom
 #pip install pyside2 pyqt5 --user
 #pip install -r requirements.txt -r dev_requirements.txt
-cd ..
+cd $INSTALL_DIR
 
 #QML2_IMPORT_PATH=/path/to/qmlAlembic/install/qml
 #QT_PLUGIN_PATH=/path/to/QtOIIO/install
 #QML2_IMPORT_PATH=/path/to/QtOIIO/install/qml
-
-
+#-------------------------------------------
+#               TRY TO RUN
 #-------------------------------------------
 
 ALICEVISION_SENSOR_DB=/home/ammar/Documents/3dParty/AliceVision/src/aliceVision/sensorDB
