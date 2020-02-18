c8b1ad33afea5a1d2b3272805393bdc40db9f389
Ammar Qammaz <ammarkov@ics.forth.gr>
2020-02-18 15:13:48 +0200

    ...

--- Setup/lock.sh
+++ Setup/lock.sh
@@ -4,7 +4,9 @@ NUMBEROFSCREENSAVERDAEMONSRUNNING=`ps -A | grep xscreensaver | wc -l`
 
 if [ "$NUMBEROFSCREENSAVERDAEMONSRUNNING" -eq "0" ]; then
      #if xscreensaver is not running then run it..! 
+     echo "XScreensaver not running, starting it up" 
      xscreensaver -nosplash&
+     sleep 1
 fi
 
 
