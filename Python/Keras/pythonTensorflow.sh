94c6a0405d6a8073c868c44169c2846398c3cf54
Ammar Qammaz <ammarkov@ics.forth.gr>
2020-03-09 11:49:39 +0200

    ..

--- Python/Keras/pythonTensorflow.sh
+++ Python/Keras/pythonTensorflow.sh
@@ -4,12 +4,9 @@ sudo apt-get install python3-venv python3-tk
 python3 -m venv tensorflow
 source tensorflow/bin/activate
 pip install numpy
-pip install tensorflow
-#pip install tensorflow_gpu
-pip install keras 
-pip install pillow
-pip install matplotlib 
-pip install pydot
+#pip install tensorflow
+pip install tensorflow_gpu
+pip install keras pillow matplotlib pydot
 
 
 exit 0
