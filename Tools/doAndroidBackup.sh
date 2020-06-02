854a3471b7cd5d2a002d6f5e78827b230e424727
Ammar Qammaz <ammarkov@ics.forth.gr>
2020-06-02 13:18:38 +0300

    simpler better android backup script..

--- Tools/doAndroidBackup.sh
+++ Tools/doAndroidBackup.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
 ADBAPP="adb"
 
-cd DCIM/Camera
+#mkdir -p DCIM/Camera
+#cd DCIM/Camera
+#sudo $ADBAPP shell ls /sdcard/DCIM/Camera/*.mp4 | tr '\r' ' ' | xargs -n1 sudo $ADBAPP pull  
+#sudo $ADBAPP shell ls /sdcard/DCIM/Camera/*.jpg | tr '\r' ' ' | xargs -n1 sudo $ADBAPP pull  
+#cd "$DIR"
+#sudo chown ammar:ammar -R DCIM/Camera/
 
-sudo $ADBAPP shell ls /sdcard/DCIM/Camera/*.mp4 | tr '\r' ' ' | xargs -n1 sudo $ADBAPP pull  
- sudo $ADBAPP shell ls /sdcard/DCIM/Camera/*.jpg | tr '\r' ' ' | xargs -n1 sudo $ADBAPP pull  
+#We will sync all this directories from the android device to the local file system
+TASKLIST="Download/X Download DCIM/Camera DCIM/100ANDRO DCIM/Facebook Plumble Pictures/Instagram Pictures/Messenger Pictures/Screenshots Pictures/mydlink Pictures/panoramas Pictures/workplace_chat bluetooth Videos Documents"
+for TASK in $TASKLIST
+do
+  cd "$DIR"
+  mkdir -p $TASK
+  cd $TASK
+  sudo $ADBAPP shell ls /sdcard/$TASK/* | tr '\r' ' ' | xargs -n1 sudo $ADBAPP pull  
+  cd "$DIR"
+  sudo chown ammar:ammar -R $TASK/
+done
 
-
-exit 0
 
 exit 0
