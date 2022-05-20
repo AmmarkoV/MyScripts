8b4f35c6f695d8e8334d753657bc51151e5d2637
Ammar Qammaz <ammarkov@gmail.com>
2022-05-21 01:19:30 +0300

    ...

--- Setup/installPython3.7.sh
+++ Setup/installPython3.7.sh
@@ -2,13 +2,25 @@
 
 cd ~
 
-sudo apt install libffi-dev
+sudo apt install libffi-dev libssl-dev openssl
+
+
+#Modules/Setup.dist
+# _socket socketmodule.c <- uncomment this line
+
+# Socket module helper for SSL support; you must comment out the other
+# socket line above, and possibly edit the SSL variable:
+#SSL=/usr/local/ssl
+#_ssl _ssl.c \ <- uncomment this line
+# -DUSE_SSL -I$(SSL)/include -I$(SSL)/include/openssl \ <- uncomment this line
+# -L$(SSL)/lib -lssl -lcrypto  <- uncomment this line
+
 
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar xzf Python-3.7.0.tgz
 cd Python-3.7.0
 ./configure --enable-optimizations
-make altinstall
+sudo make altinstall
 rm /usr/src/Python-3.7.0.tgz
 
 exit 0
