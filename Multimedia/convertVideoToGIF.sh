6121d7a143afb30bc49c02b72896854ac741d467
Ammar Qammaz <ammarkov@gmail.com>
2023-10-26 18:26:09 +0300

    updated convertvideo to GIF

--- Multimedia/convertVideoToGIF.sh
+++ Multimedia/convertVideoToGIF.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ffmpeg -i $1 -r 6 -pix_fmt rgb24 "$2-temp.gif" 
-convert -layers Optimize "$2-temp.gif" "$2.gif"
+#sudo pluma /etc/ImageMagick-6/policy.xml
+#increase 
+#   <policy domain="resource" name="memory" value="4256MiB"/>
+# <!-- <policy domain="system" name="max-memory-request" value="4256MiB"/> -->
+ #-vf scale="720:-1" -t 00:00:23
+ffmpeg -i $1  -r 6 -pix_fmt rgb24 "$2-temp.gif"  
+convert -limit memory 4GiB -layers Optimize "$2-temp.gif" "$2.gif"
 rm "$2-temp.gif"
 
 
