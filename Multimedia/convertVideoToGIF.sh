47c159be6f19f1df4ee8688a5e48d0534732e870
Ammar Qammaz <ammarkov@gmail.com>
2023-11-22 15:43:51 +0200

    ...

--- Multimedia/convertVideoToGIF.sh
+++ Multimedia/convertVideoToGIF.sh
@@ -4,8 +4,10 @@
 #increase 
 #   <policy domain="resource" name="memory" value="4256MiB"/>
 # <!-- <policy domain="system" name="max-memory-request" value="4256MiB"/> -->
- #-vf scale="720:-1" -t 00:00:23
-ffmpeg -i $1  -r 6 -pix_fmt rgb24 "$2-temp.gif"  
+#ffmpeg -y -i argaleios.mp4 -r 5 -vf scale="720:-1" -ss 00:00:10  -t 00:00:23 -pix_fmt rgb8 argaleios.gif
+#    rescale            start       duration 
+#-vf scale="720:-1" -ss 00:00:10 -t 00:00:23
+ffmpeg -i $1  -y -r 6 -pix_fmt rgb8 "$2-temp.gif"  
 convert -limit memory 4GiB -layers Optimize "$2-temp.gif" "$2.gif"
 rm "$2-temp.gif"
 
--- Setup/cmake_install.sh
+++ Setup/cmake_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-VERSION="3.20.2"
+VERSION="3.27.8" #Last stable version in 22/11/23
+
+#sudo apt-get remove cmake cmake-data
 
 sudo apt install build-essential libssl-dev
 wget https://github.com/Kitware/CMake/releases/download/v$VERSION/cmake-$VERSION.tar.gz
