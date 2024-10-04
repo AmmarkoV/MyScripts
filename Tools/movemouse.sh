7d62a25b7befa79db81086dc06b13a9ed8cc2b6c
Ammar Qammaz <ammarkov@ics.forth.gr>
2024-10-04 11:39:39 +0300

    add a script that moves the mouse

---
+++ Tools/movemouse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Infinite loop to move the mouse every second
+while true; do
+  # Get the current mouse position
+  eval "$(xdotool getmouselocation --shell)"
+  
+  # Move the mouse by 1 pixel and then back to its original position
+  xdotool mousemove $((X+1)) $Y
+  xdotool mousemove $X $Y
+
+  # Wait for 1 second
+  sleep 1
+done
