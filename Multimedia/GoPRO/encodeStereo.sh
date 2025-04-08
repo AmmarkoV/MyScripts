306ccc2cfad5ac046cf8b05012f41c20f52dfc00
Ammar Qammaz <ammarkov@gmail.com>
2025-04-08 22:37:45 +0300

    ...

--- Multimedia/GoPRO/encodeStereo.sh
+++ Multimedia/GoPRO/encodeStereo.sh
@@ -3,14 +3,13 @@
 #sudo apt install mkvtoolnix ffmpeg
 
 
-FILE="GX010005.MP4"
-VERTICALCROP="40"
+FILE="GX010006.MP4" 
 
 ffmpeg -i left/$FILE -i right/$FILE -filter_complex "\
-[0:v]transpose=1,hqdn3d=0:0:8:8,eq=contrast=1.2:brightness=-0.05:saturation=1.25,\
-     crop=in_w:in_h-$VERTICALCROP:0:$VERTICALCROP[l]; \
-[1:v]transpose=1,hqdn3d=0:0:8:8,eq=contrast=1.2:brightness=-0.05:saturation=1.25,\
-     crop=in_w:in_h-$VERTICALCROP:0:$VERTICALCROP[r]; \
+[0:v]hqdn3d=0:0:8:8,eq=contrast=1.2:brightness=-0.05:saturation=1.25,\
+     crop=in_w:in_h:0:0[l]; \
+[1:v]hqdn3d=0:0:8:8,eq=contrast=1.2:brightness=-0.05:saturation=1.25,\
+     crop=in_w:in_h:0:0[r]; \
 [l][r]hstack=inputs=2,format=yuv420p[v]" \
 -map "[v]" -c:v libx264 -crf 18 -metadata:s:v:0 stereo_mode=left_right -y -threads 0 c$FILE.mkv
 
