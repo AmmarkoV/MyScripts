124ca11d7ff390c2e5e2be850ff8e0dce1c61842
Ammar Qammaz <ammarkov@gmail.com>
2025-05-18 17:02:08 +0300

    ...

--- Tools/Arxiv/cronTask.sh
+++ Tools/Arxiv/cronTask.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+
+# 
+#crontab -l > mycron
+#echo "30 22 * * * /home/ammar/public_html/news/cronTask.sh" >> mycron
+#crontab mycron
+#rm mycron
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 rm arxiv_*.html
+
+source venv/bin/activate
 
 python3 getArxivNews.py
 exit 0
