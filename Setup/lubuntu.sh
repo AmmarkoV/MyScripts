91f6b1e5dc912301c40fdb7e9eb7ecf4dee3477e
Ammar Qammaz <ammarkov@ics.forth.gr>
2025-05-21 12:45:31 +0300

    ...

--- Setup/lubuntu.sh
+++ Setup/lubuntu.sh
@@ -451,7 +451,7 @@ fi
 #Don't use firefox snap
 #sudo snap remove firefox
 #sudo install -d -m 0755 /etc/apt/keyrings
-#wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo #tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+#wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 #echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 #echo '
 #Package: *
@@ -459,9 +459,7 @@ fi
 #Pin-Priority: 1000
 #' | sudo tee /etc/apt/preferences.d/mozilla
 #sudo apt update && sudo apt install firefox
-
-
-
+ 
 
 #Install Steam
 #Steam needs 32bit libc
