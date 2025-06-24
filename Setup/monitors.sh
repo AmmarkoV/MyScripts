632aa10b492e6864cbab71c937fed659ac59380f
Ammar Qammaz <ammarkov@gmail.com>
2025-06-24 21:53:53 +0300

    add monitor configuration of my PC

---
+++ Setup/monitors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Use xrandr --query to see configuration
+xrandr --output DP-3 --mode 1920x1080 --pos 0x0 --primary --output HDMI-0 --mode 1920x1080 --pos 1920x0 --output DP-1 --mode 1920x1080 --pos 3840x0 
+
+exit 0
