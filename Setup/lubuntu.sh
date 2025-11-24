c19985e86efe9e0e9c5398f2fecc344fe9a97bda
Ammar Qammaz <ammarkov@gmail.com>
2025-11-24 15:50:11 +0200

    ...

--- Setup/lubuntu.sh
+++ Setup/lubuntu.sh
@@ -481,6 +481,13 @@ fi
 
 
 
+#Spotify
+#curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+#echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+#sudo apt-get update && sudo apt-get install spotify-client
+
+#Teamviewer
+
 #Don't use firefox snap
 #sudo snap remove firefox
 #sudo install -d -m 0755 /etc/apt/keyrings
